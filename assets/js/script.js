a00fd2dfc75388ecea3a15170d279c014c44cb54
jaycee808 <jamie.clark@live.co.uk>
2023-02-15 16:28:54 +0000

    added weekly tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // variables for current day and time
-var currentDay = moment().format('dddd Do MMMM YYYY');
+var currentDay = moment().format('dddd Do MMMM');
 
 // display current date and time to the screen
 $('#currentDay').append(currentDay);
